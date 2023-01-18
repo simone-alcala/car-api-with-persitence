e81c8320ac374a88dd6d5312f590df6c77a6bb5d
simone.alcala <simone.alcala@gmail.com>
2023-01-18 14:39:08 -0300

    Feat: Car entity

---
+++ src/main/java/com/aceleracao/modelaai/model/Car.java
@@ -0,0 +1,45 @@
+package com.aceleracao.modelaai.model;
+
+import com.aceleracao.modelaai.dto.CarDTO;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+public class Car {
+
+  public Car(CarDTO data) {
+    this.modelo = data.modelo();
+    this.fabricante = data.fabricante();
+    this.dataFabricacao = data.dataFabricacao();
+    this.valor = data.valor();
+    this.anoModelo = data.anoModelo();
+  }
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(length = 25, nullable = false)
+  private String modelo;
+
+  @Column(length = 25, nullable = false)
+  private String fabricante;
+
+  @Column(length = 10, nullable = false)
+  private String dataFabricacao;
+
+  @Column(nullable = false)
+  private Double valor;
+
+  @Column(nullable = false)
+  private Integer anoModelo;
+
+}
