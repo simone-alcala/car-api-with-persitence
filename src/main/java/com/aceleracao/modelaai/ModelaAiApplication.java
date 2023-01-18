e5318810568c16ed14f15477062798a69c4b3a69
simone.alcala <simone.alcala@gmail.com>
2023-01-18 14:37:45 -0300

    Chore: First commit

---
+++ src/main/java/com/aceleracao/modelaai/ModelaAiApplication.java
@@ -0,0 +1,13 @@
+package com.aceleracao.modelaai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aceleracao/modelaai/ModelaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aceleracao.modelaai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
